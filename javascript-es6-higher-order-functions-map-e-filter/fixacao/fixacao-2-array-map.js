bb96427497aa95fa996f9e5231dc3ab26e43ce4e
Pedro Silva Buente <pedrobuente@gmail.com>
2021-08-02 11:18:38 -0300

    fixação

---
+++ javascript-es6-higher-order-functions-map-e-filter/fixacao/fixacao-2-array-map.js
@@ -0,0 +1,22 @@
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
